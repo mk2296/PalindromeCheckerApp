377ebf274b2d89358f3cac628f0912015e400567
mk2296 <mk2296@srmist.edu.in>
2026-02-27 10:07:00 +0530

    uc3 updated with uc4

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.print("Input text:");
-        String input=sc.nextLine();
-        String copyString="";
-        for(int i=input.length()-1;i>=0;i--){
-            copyString=copyString+input.charAt(i);
-
+        String input="radar";
+        char[] chars=input.toCharArray();
+        int start=0;
+        int end=chars.length-1;
+        boolean isPalindrome=true;
+        while(start<end){
+            if (chars[start]!=chars[end]){
+                isPalindrome=false;
+            }
+            start++;
+            end--;
         }
-        if(input.equals(copyString)){
-            System.out.println("Is it a palindrome?: true");
-        }
-        else{
-            System.out.println("Is it a palindrome?: false");
-        }
+        System.out.println("is it a palindrome?:"+isPalindrome);
 
 
     }
